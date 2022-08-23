56115b86eec8e7a7c2f4f76bf1913ccf68f0b1f1
Thannzz <thanigai128@gmail.com>
2022-08-23 16:06:14 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const containerStyle = {
+  width: "100%",
+  height: "50px",
+  backgroundColor: "#03A9F4",
+};
+
+const Navbar = () => {
+  return <Box style={containerStyle}></Box>;
+};
+
+export default Navbar;
---
+++ src/Routes/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import { Box } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
+
+export default Home;
