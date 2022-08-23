0ae1917ac7c6109cf6eff214b2b69947de5218d4
Thannzz <thanigai128@gmail.com>
2022-08-23 16:21:44 +0530

    day-1 first  commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,40 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 
 const containerStyle = {
   width: "100%",
   height: "50px",
   backgroundColor: "#03A9F4",
 };
+const innerBox = {
+  width: "80%",
+  border: "1px solid red",
+  height: "100%",
+  margin: "auto",
+};
 
 const Navbar = () => {
-  return <Box style={containerStyle}></Box>;
+  return (
+    <Box style={containerStyle}>
+      <Box style={innerBox}>
+        <Box
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            width: "150px",
+            height: "100%",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            style={{ width: "138px", height: "20px" }}
+            src="https://wassets.hscicdn.com/static/images/logo.png"
+            alt="espnLogo"
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Navbar;
